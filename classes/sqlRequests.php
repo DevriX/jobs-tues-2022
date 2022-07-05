d4b2eb88de9bdb3be69d80660b00bbcba9efdd8d
NemaGrijiVelkataDviji <vili.levski@abv.bg>
2022-07-05 19:51:48 +0300

    fixed issues

--- classes/sqlRequests.php
+++ classes/sqlRequests.php
@@ -7,36 +7,15 @@ class Requests{
 
     function connectDB(){
         $conn = mysqli_connect("localhost", "fustuk", "123456", "jobs_devrix");
-        if($conn){
-            echo("Connected successfully");
-        }else{
+        if(!$conn){
             echo("Error connecting");
         }
         
         mysqli_select_db($conn, "jobs_devrix") or die(mysqli_error($conn));
         return $conn;
     }
-    
-    function Insert($object){
-        $conn = connectDB();
-        if($object.get_class() == "users"){
-            $sql = 'INSERT INTO  users  values()';
-        }
-        else if($object.get_class() == "jobs"){
-            $sql = 'INSERT INTO  jobs  values()';
-        }
-        else if($object.get_class() == "categories"){
-            $sql = 'INSERT INTO  categories  values()';
-        }
-        else if($object.get_class() == "applications"){
-            $sql = 'INSERT INTO  applications  values()';
-        }
-        
-        mysqli_querry($conn, $sql);
+
+
     }
 
 }
-
-
-
-?>
