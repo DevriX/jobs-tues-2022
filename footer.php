967a1382e90c17dfc214d2d7f2cfa8aedf3e2dc0
bozhiinov <bozhiinov@gmail.com>
2022-07-05 15:15:47 +0300

    header and footer files fix

--- footer.php
+++ footer.php
@@ -1,30 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
+<div class="site-wrapper">
 
-    <head>
-
-    </head>
-
-    <body>
-        <div class="site-wrapper">
-    
-            <footer class="site-footer">
-                <div class="row">
-                    <p>Copyright 2020 | Developer links: 
-                        <a href="/index.php">Home</a>,
-                        <a href="/dashboard.php">Jobs Dashboard</a>,
-                        <a href="/single.php">Single</a>,
-                        <a href="/login.php">Login</a>,
-                        <a href="/register.php">Register</a>,
-                        <a href="/submissions.php">Submissions</a>,
-                        <a href="/apply-submission.php">Apply Submission</a>,
-                        <a href="/view-submission.php">View Submission</a>,
-                        <a href="/actions-job.php">Create-Edit Job</a>,
-                        <a href="/category-dashboard.php">Category Dashboard</a>,
-                        <a href="/profile.php">My Profile</a>
-                    </p>
-                </div>
-            </footer>
+    <footer class="site-footer">
+        <div class="row">
+            <p>Copyright 2020 | Developer links: 
+                <a href="/index.php">Home</a>,
+                <a href="/dashboard.php">Jobs Dashboard</a>,
+                <a href="/single.php">Single</a>,
+                <a href="/login.php">Login</a>,
+                <a href="/register.php">Register</a>,
+                <a href="/submissions.php">Submissions</a>,
+                <a href="/apply-submission.php">Apply Submission</a>,
+                <a href="/view-submission.php">View Submission</a>,
+                <a href="/actions-job.php">Create-Edit Job</a>,
+                <a href="/category-dashboard.php">Category Dashboard</a>,
+                <a href="/profile.php">My Profile</a>
+            </p>
         </div>
-    </body>
-    </html>
+    </footer>
+</div>
