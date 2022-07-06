43f7f474f5268ecd270b49e99359401869288061
abozhiinov <105458443+abozhiinov@users.noreply.github.com>
2022-07-06 18:56:14 +0300

    A bunch of changes on "job submissions" and "apply now" (#32)
    * get input and error handling / check for valid email and phone number
    
    * file checking for pdf or dockx only
    
    * create job made
    
    * numeric check for salary in actions-job.php
    
    * file check commented cause it gives an error
    
    * fixes
    
    * fixes

--- apply-submission.php
+++ apply-submission.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	
-</head>
+
 <body>
-<?php include 'header.php';?>
+	<?php 
+		include 'header.php';
+	?>
 	<div class="site-wrapper">
 
 		<main class="site-main">
@@ -16,25 +16,31 @@
 								<h2 class="heading-title">Submit application to
 									Company Name</h2>
 							</div>
-							<form>
+							<form method="post" action="">
 								<div class="flex-container justified-horizontally flex-wrap">									
 									<div class="form-field-wrapper width-medium">
-										<input type="text" placeholder="First Name*"/>
+										<input type="text" name="firstname" placeholder="First Name*"/>
+										<span class="error">  <?php //echo $err["first_name_err"];?> </span>  
 									</div>
 									<div class="form-field-wrapper width-medium">
-										<input type="text" placeholder="Last Name*"/>
+										<input type="text" name="lastname" placeholder="Last Name*"/>
+										<span class="error">  <?php //echo $err["last_name_err"];?> </span>  
 									</div>
 									<div class="form-field-wrapper width-medium">
-										<input type="text" placeholder="Email*"/>
+										<input type="text" name="email" placeholder="Email*"/>
+										<span class="error">  <?php //echo $err["email_err"];?> </span>  
 									</div>
 									<div class="form-field-wrapper width-medium">
-										<input type="text" placeholder="Phone Number"/>
+										<input type="text" name="phone" placeholder="Phone Number"/>
+										<span class="error">  <?php //echo $err["phone_err"];?> </span> 
 									</div>			
 									<div class="form-field-wrapper width-large">
-										<textarea placeholder="Custom Message*"></textarea>
+									<span class="error" >  <?php //echo $err["message_err"];?> </span>  
+										<textarea placeholder="Custom Message*" name="message"></textarea>
 									</div>
 									<div class="form-field-wrapper width-large">
-										<input type="file" />
+										<input type="file" name="file_input"/>
+										<span class="error" >  <?php //echo $err["file_err"];?> </span> 
 									</div>
 								</div>	
 								<button class="button">
