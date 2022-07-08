934f91812d00bfe2aad981f86c07435f7b924f0f
Alexandra Stancheva <66909315+allks12@users.noreply.github.com>
2022-07-08 17:50:01 +0300

    Working search without pagination for now!

--- index.php
+++ index.php
@@ -33,15 +33,7 @@
 								<form method = "get">	 
 									<input class="search-form-input" type="text" value="" placeholder="Search…" name="search">
 								</form>
-
-								<?php
-									$is_searched = false;
-									
-									if(isset($_GET["search"])){
-										$is_searched = true;
-										$key_word = $_GET["search"];
-									}
-								?>
+								
 							</div> 
 						</div>
 						<div class="filter-wrapper">
@@ -56,7 +48,39 @@
 						</div>
 					</div>
 					<ul class="jobs-listing">
-						<?php 
+					<?php
+						$is_searched = false;
+							if(isset($_GET["search"])){
+								$is_searched = true;				
+								$key_word = $_GET["search"];
+								$request_job_info = $conn->query("SELECT j.title, j.location, DATEDIFF(CURDATE(), j.date_posted) AS 'date', u.company_name, u.company_image
+														FROM jobs as j JOIN users as u on u.id = j.user_id
+														HAVING (j.title LIKE '%".$key_word."%')  
+														ORDER BY date_posted DESC");
+								
+								while($row = mysqli_fetch_array($request_job_info, MYSQLI_BOTH)) {
+									$company_image_path = "/uploads/company_images/".$row["company_image"];?>
+									<li class="job-card">
+										<div class="job-primary">
+											<h2 class="job-title"><a href="#"><?php echo $row["title"];?></a></h2>
+											<div class="job-meta">
+												<a class="meta-company" href="#"><?php echo $row["company_name"];?></a>
+												<span class="meta-date">Posted <?php echo time_diff_mesage($row["date"]);?></span>
+											</div>
+											<div class="job-details">
+												<span class="job-location"><?php echo $row["location"];?></span>
+												<span class="job-type">Contract staff</span>
+											</div>
+										</div>
+										<div class="job-logo">
+											<div class="job-logo-box">
+												<img src=<?php echo $company_image_path;?> alt="">
+											</div>
+										</div>
+									</li>
+							<?php } 
+							} 
+
 							function time_diff_mesage($diff){
 								switch($diff){
 									case 0:
@@ -79,67 +103,57 @@
 							$page_first_result = ($page-1) * $limit;
 
 							
-							if($is_searched == true){	
-								$request_job_info = $conn->query("SELECT j.title, j.location, DATEDIFF(CURDATE(), j.date_posted) AS 'date', u.company_name, u.company_image
-																FROM jobs as j JOIN users as u on u.id = j.user_id
-																HAVING (j.title LIKE '%".$key_word."%')  
-																ORDER BY date_posted DESC 
-																LIMIT $page_first_result, $limit");
-								$num_rows = mysqli_num_rows ($conn->query("SELECT * FROM jobs WHERE (title LIKE '%".$key_word."%')"));
-								/*$is_searched = false;
-								$key_word = "";*/
-							}
-							else{
-								$request_job_info = $conn->query("SELECT j.title, j.location, DATEDIFF(CURDATE(), j.date_posted) AS 'date', u.company_name, u.company_image
-																FROM jobs as j JOIN users as u on u.id = j.user_id 
-																ORDER BY date_posted DESC 
-																LIMIT $page_first_result, $limit");
+							if($is_searched == false){	
+									$request_job_info = $conn->query("SELECT j.title, j.location, DATEDIFF(CURDATE(), j.date_posted) AS 'date', u.company_name, u.company_image
+																	FROM jobs as j JOIN users as u on u.id = j.user_id 
+																	ORDER BY date_posted DESC 
+																	LIMIT $page_first_result, $limit");
+									
+									$num_rows = mysqli_num_rows ($conn->query("SELECT * FROM jobs"));
 								
-								$num_rows = mysqli_num_rows ($conn->query("SELECT * FROM jobs"));
-							}
-							
-							
-							
-							$page_total = ceil($num_rows / $limit);
 
-							while($row = mysqli_fetch_array($request_job_info, MYSQLI_BOTH)) {
-								$company_image_path = "/uploads/company_images/".$row["company_image"];?>
-								<li class="job-card">
-									<div class="job-primary">
-										<h2 class="job-title"><a href="#"><?php echo $row["title"];?></a></h2>
-										<div class="job-meta">
-											<a class="meta-company" href="#"><?php echo $row["company_name"];?></a>
-											<span class="meta-date">Posted <?php echo time_diff_mesage($row["date"]);?></span>
-										</div>
-										<div class="job-details">
-											<span class="job-location"><?php echo $row["location"];?></span>
-											<span class="job-type">Contract staff</span>
-										</div>
-									</div>
-									<div class="job-logo">
-										<div class="job-logo-box">
-											<img src=<?php echo $company_image_path;?> alt="">
-										</div>
-									</div>
-								</li>
-						<?php } ?>		
-							
-					</ul>
-					<div class="jobs-pagination-wrapper">
-						<div class="nav-links">
-							<?php 
-								   for ($i = 1; $i <= $page_total; $i++) {
+								
+									$page_total = ceil($num_rows / $limit);
 
-										if($i == $page){
-											printf("<a class='page-numbers current' %shref='index.php?page=%u'>%u</a>", 
-												$i==$page ? : "", $i, $i );
-										}
-										else{
-											printf("<a class='page-numbers' %shref='index.php?page=%u'>%u</a>", 
-												$i==$page ? : "", $i, $i );
-										}
-									}
-							?>
+									while($row = mysqli_fetch_array($request_job_info, MYSQLI_BOTH)) {
+										$company_image_path = "/uploads/company_images/".$row["company_image"];?>
+										<li class="job-card">
+											<div class="job-primary">
+												<h2 class="job-title"><a href="#"><?php echo $row["title"];?></a></h2>
+												<div class="job-meta">
+													<a class="meta-company" href="#"><?php echo $row["company_name"];?></a>
+													<span class="meta-date">Posted <?php echo time_diff_mesage($row["date"]);?></span>
+												</div>
+												<div class="job-details">
+													<span class="job-location"><?php echo $row["location"];?></span>
+													<span class="job-type">Contract staff</span>
+												</div>
+											</div>
+											<div class="job-logo">
+												<div class="job-logo-box">
+													<img src=<?php echo $company_image_path;?> alt="">
+												</div>
+											</div>
+										</li>
+								<?php  } ?>		
+									
+								</ul>
+								<div class="jobs-pagination-wrapper">
+									<div class="nav-links">
+										<?php 
+											for ($i = 1; $i <= $page_total; $i++) {
+
+													if($i == $page){
+														printf("<a class='page-numbers current' %shref='index.php?page=%u'>%u</a>", 
+															$i==$page ? : "", $i, $i );
+													}
+													else{
+														printf("<a class='page-numbers' %shref='index.php?page=%u'>%u</a>", 
+															$i==$page ? : "", $i, $i );
+													}
+												}
+							
+							}?>
 						</div>
 					</div>
 				</div>
