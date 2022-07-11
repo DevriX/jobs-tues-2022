96d0f2fed324db069278ba4fbdf77eee61374209
abozhiinov <abozhiinov@gmail.com>
2022-07-11 19:10:05 +0300

    approve/reject optimized

--- dashboard.php
+++ dashboard.php
@@ -6,20 +6,20 @@
 include 'header.php';
 
 if($_SERVER["REQUEST_METHOD"] == "GET"){
-	if(!empty($_GET["approved"])){
-		$approve_request = "UPDATE jobs SET status = 1 WHERE jobs.id = " . $_GET['job_id'] . "";
+	if(!empty($_GET["status"])){
+		if($_GET['status'] == "a"){
+			$approve_request = "UPDATE jobs SET status = 1 WHERE jobs.id = " . $_GET['job_id'] . "";
 
-		if ($conn->query($approve_request) === FALSE) {
-			echo "Error: " . $approve_request . "<br>" . $conn->error;
-		}
-	}
-
-	if(!empty($_GET['rejected'])){
-		$reject_request = "UPDATE jobs SET status = 0 WHERE jobs.id = " . $_GET['job_id'] . "";
-	
-			
-		if ($conn->query($reject_request) === FALSE) {
-			echo "Error: " . $delete_request . "<br>" . $conn->error;
+			if ($conn->query($approve_request) === FALSE) {
+				echo "Error: " . $approve_request . "<br>" . $conn->error;
+			}
+		} else {
+			$reject_request = "UPDATE jobs SET status = 0 WHERE jobs.id = " . $_GET['job_id'] . "";
+		
+				
+			if ($conn->query($reject_request) === FALSE) {
+				echo "Error: " . $delete_request . "<br>" . $conn->error;
+			}
 		}
 	}
 }
@@ -51,8 +51,8 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
 								<div class="filter-wrapper">
 									<div class="filter-field-wrapper">
 										<select name="menu">
-											<option name="by_date" value="1">Date</option>
-											<option name="alphabetically" value="2">Alphabetically</option>
+											<option value="1">Date</option>
+											<option value="2">Alphabetically</option>
 										</select>
 									</div>
 								</div>
@@ -90,9 +90,9 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
 								<div class="job-actions">
 									<form method="post">
 										<?php if($row['status'] == 0){ ?>
-											<a href="<?php echo $_SERVER["PHP_SELF"]?>?job_id=<?php echo $row['main_id']; ?>&approved=true" name="approved"> Approve </a>
+											<a href="<?php echo $_SERVER["PHP_SELF"]?>?job_id=<?php echo $row['main_id']; ?>&status=a"> Approve </a>
 										<?php } else { ?>
-											<a href="<?php echo $_SERVER["PHP_SELF"]?>?job_id=<?php echo $row['main_id']; ?>&rejected=true" name="rejected">Reject</a>
+											<a href="<?php echo $_SERVER["PHP_SELF"]?>?job_id=<?php echo $row['main_id']; ?>&status=r">Reject</a>
 										<?php } ?>
 									</form>
 								</div>
