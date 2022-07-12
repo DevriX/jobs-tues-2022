23e065cb6bfaa2d438bd67f206e7ce75016ad341
abozhiinov <105458443+abozhiinov@users.noreply.github.com>
2022-07-12 15:15:17 +0300

    create job added in header & search/filter added in jobs dashboard (#46)
    * get input and error handling / check for valid email and phone number
    
    * file checking for pdf or dockx only
    
    * create job made
    
    * numeric check for salary in actions-job.php
    
    * file check commented cause it gives an error
    
    * fixes
    
    * fixes
    
    * massive bug fixed
    
    * job with no applications view added
    
    * edit job added
    
    * create/edit job optimized
    
    * empty instead of isset
    
    * approve/reject optimized
    
    * approve/reject optimized
    
    * create job added in header & search/filter added in jobs dashboard
    
    * do not forget search and filter values

--- dashboard.php
+++ dashboard.php
@@ -8,13 +8,13 @@ include 'header.php';
 if($_SERVER["REQUEST_METHOD"] == "GET"){
 	if(!empty($_GET["status"])){
 		if($_GET['status'] == "a"){
-			$approve_request = "UPDATE jobs SET status = 1 WHERE jobs.id = " . $_GET['job_id'] . "";
+			$approve_request = "UPDATE jobs SET status = 1 WHERE id = " . $_GET['job_id'] . "";
 
 			if ($conn->query($approve_request) === FALSE) {
 				echo "Error: " . $approve_request . "<br>" . $conn->error;
 			}
 		} else {
-			$reject_request = "UPDATE jobs SET status = 0 WHERE jobs.id = " . $_GET['job_id'] . "";
+			$reject_request = "UPDATE jobs SET status = 0 WHERE id = " . $_GET['job_id'] . "";
 		
 				
 			if ($conn->query($reject_request) === FALSE) {
@@ -22,6 +22,29 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
 			}
 		}
 	}
+
+	$order = 'date_posted DESC';
+	if(isset($_GET['drop_down_menu']) && $_GET["drop_down_menu"] == 2){
+		$order = 'title ASC';
+	}
+
+	$search = '';
+	if(!empty($_GET['search'])){
+		$search = $_GET['search'];
+	}
+
+	if (!empty($_GET['drop_down_menu'])) {
+		$menu_value = $_GET['drop_down_menu'];
+	} else {
+		$menu_value = 1;
+	}
+
+	$request = $conn->query(
+		"SELECT *, jobs.id AS 'main_id', DATEDIFF(CURDATE(), jobs.date_posted) AS 'date' 
+		FROM jobs 
+		LEFT JOIN users ON jobs.user_id = users.id
+		HAVING title LIKE '%" . $search . "%'
+		ORDER BY " . $order . "");
 }
 
 ?>
@@ -41,38 +64,35 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
 								</li>
 							</ul>
 						</div>
-						<div class="secondary-container">
+						<div  style="display:inline-flex;" class="secondary-container">
 							<div class="flex-container centered-vertically">
-								<div class="search-form-wrapper">
-									<div class="search-form-field"> 
-										<input class="search-form-input" type="text" value="" placeholder="Search…" name="search" > 
-									</div> 
-								</div>
-								<div class="filter-wrapper">
-									<div class="filter-field-wrapper">
-										<select name="menu">
-											<option value="1">Date</option>
-											<option value="2">Alphabetically</option>
-										</select>
+								<form>
+									<div style="display:inline-flex;" class="search-form-wrapper">
+										<div class="search-form-field"> 
+											<input class="search-form-input" type="text" value="<?php if (isset($_GET['search'])) echo $_GET['search'];?>" placeholder="Search…" name="search" > 
+										</div> 
 									</div>
-								</div>
-								<div class="button-wrapper">
-									<form method="post" style="margin-left: 10px; margin-top: -15px;">
-										<button name="filter" class="button" type="submit">Submit</button>
-									</form>
-								</div>
+									<div style="display:inline-flex;" class="filter-wrapper">
+										<div class="filter-field-wrapper">
+											<select name="drop_down_menu">
+												<option value="1" <?php if ($menu_value == 1) echo 'selected="selected"'; ?>>Date</option>
+												<option value="2" <?php if ($menu_value == 2) echo 'selected="selected"'; ?>>Alphabetically</option>
+											</select>
+										</div>
+									</div>
+									<div style="display: inline-flex; margin-left: -10px;" class="button-wrapper">
+										<form method="post">
+											<button class="button" type="submit">Submit</button>
+										</form>
+									</div>
+								</form>
 							</div>
 						</div>
 					</div>
 					<ul class="jobs-listing">
 						<?php 
-							$request_job = $conn->query(
-								"SELECT *, jobs.id AS 'main_id', DATEDIFF(CURDATE(), jobs.date_posted) AS 'date' FROM jobs 
-								 LEFT JOIN users ON jobs.user_id = users.id
-								 ORDER BY date_posted DESC");
-
-							if(mysqli_num_rows($request_job) > 0){
-								while($row = mysqli_fetch_array($request_job, MYSQLI_BOTH)){
+							if(mysqli_num_rows($request) > 0){
+								while($row = mysqli_fetch_array($request, MYSQLI_BOTH)){
 						?>
 						<li class="job-card">
 							<div class="job-primary">
@@ -90,9 +110,9 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
 								<div class="job-actions">
 									<form method="post">
 										<?php if($row['status'] == 0){ ?>
-											<a href="<?php echo $_SERVER["PHP_SELF"]?>?job_id=<?php echo $row['main_id']; ?>&status=a"> Approve </a>
+											<a href="<?php echo $_SERVER["PHP_SELF"]?>?search=<?php echo $search; ?>&drop_down_menu=<?php echo $order; ?>&job_id=<?php echo $row['main_id']; ?>&status=a"> Approve </a>
 										<?php } else { ?>
-											<a href="<?php echo $_SERVER["PHP_SELF"]?>?job_id=<?php echo $row['main_id']; ?>&status=r">Reject</a>
+											<a href="<?php echo $_SERVER["PHP_SELF"]?>?search=<?php echo $search; ?>&drop_down_menu=<?php echo $order; ?>&job_id=<?php echo $row['main_id']; ?>&status=r">Reject</a>
 										<?php } ?>
 									</form>
 								</div>
