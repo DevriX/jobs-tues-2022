6d3300754354697fa0cf42b6d0470c8adad661ba
skrrrrrrrrrrrrrt <iliqnemilov11@gmail.com>
2022-07-13 02:34:38 -0700

    added single view

--- single.php
+++ single.php
@@ -9,38 +9,37 @@ $db = new Requests;
 $conn = $db->connectDB();
 
 //var_dump($_GET);
-$job_id = $_GET['job-id'];
+$job_id = $_GET['job_id'];
 
 //var_dump($job_id);
 
 if($job_id != null){
-	$statement_job = "SELECT * FROM jobs WHERE id = " . $_GET['job-id'];
+	$sql = "SELECT * 
+			FROM jobs 
+			LEFT JOIN users ON jobs.user_id=users.id 
+			WHERE jobs.id = " . $_GET['job_id'] . " ";
+	$result = mysqli_query($conn, $sql);
+	if ($result->num_rows > 0) {
+		$row = $result->fetch_assoc();
+		if(empty($row)){
+			echo "0 results";
+		}
+	}
+	//var_dump($row);
 
-	$result_job = mysqli_query($conn, $statement_job);
-
-	$job = mysqli_fetch_all($result_job,MYSQLI_ASSOC);
-}
-// if($job_id == null){
-// 	$statement = "SELECT * FROM jobs";
-
-// 	$result = mysqli_query($conn, $statement);
-
-// 	$job = mysqli_fetch_all($result,MYSQLI_ASSOC);
-// }
-
-if($job_id != null){
 	$statement_related_jobs = 
 			"SELECT *
-			FROM jobs_categories Categories
-			LEFT JOIN jobs Jobs ON Categories.job_id = Jobs.id
-			WHERE Jobs.id != $job_id
-			LIMIT 0, 3";
+			FROM jobs_categories 
+			LEFT JOIN jobs ON jobs.id = jobs_categories.job_id 
+			LEFT JOIN users ON jobs.user_id=users.id
+			WHERE  job_id != $job_id AND category_id 
+			IN (SELECT subquery.category_id FROM jobs_categories subquery WHERE job_id = $job_id )
+			ORDER BY rand() ";
 
-	$results = mysqli_query($conn, $statement_related_jobs);
+	$results_related_jobs = mysqli_query($conn, $statement_related_jobs);
 
-	$related_jobs = mysqli_fetch_all($results,MYSQLI_ASSOC);
+	$related_jobs = mysqli_fetch_all($results_related_jobs,MYSQLI_ASSOC);
 }
-//var_dump($related_jobs);
 
 ?>
 
@@ -55,24 +54,20 @@ if($job_id != null){
 								<div class="job-card">
 									<div class="job-primary">
 										<header class="job-header"> 
-												<?php foreach($job as $i => $arguments){ ?>
-													<h2 type="" class="job-title"><?php echo $arguments['title'] ?></h2>
+													<h2 class="job-title"><?php echo $row['title'] ?></h2>
 													<div class="job-meta">
-														<a class="meta-company" href="#">Company Awesome Ltd.</a>
-														<span class="meta-date">Posted on: <?php echo $arguments['date_posted'] ?></span>
+														<a class="meta-company" href="#"><?php echo $row["company_name"]; ?></a>
+														<span class="meta-date">Posted on: <?php echo $row['date_posted'] ?></span>
 													</div>
 													<div class="job-details">
-														<span class="job-location"><?php echo $arguments['location'] ?></span>
+														<span class="job-location"><?php echo $row['location'] ?></span>
 														<span class="job-type">Contract staff</span>
-														<span class="job-price"><?php echo $arguments['salary'] ?> Lv.</span>
+														<span class="job-price"><?php echo $row['salary'] ?> Lv.</span>
 													</div>
-												<?php } ?>
 										</header>
-											<?php foreach($job as $i => $arguments){ ?>
 												<div class="job-body">
-													<P><?php echo $arguments['description']?> </P>
+													<P><?php echo $row['description']?> </P>
 												</div>
-											<?php } ?>
 									</div>
 								</div>
 							</div>
@@ -82,7 +77,7 @@ if($job_id != null){
 										<img src="https://i.imgur.com/ZbILm3F.png" alt="">
 									</div>
 								</div>
-								<a href="apply-submission.php" class="button button">Apply now</a>
+								<a href="apply-submission.php?job_id=<?php echo($_GET['job_id']) ?>" class="button button">Apply now</a>
 								<a href="#" class="button button">Edit now</a>
 								<a href="https://www.CompanyAwesomeLtd..com/">Link to the company</a>
 							</aside>
@@ -97,9 +92,9 @@ if($job_id != null){
 									<ul class="jobs-listing">
 										<li class="job-card">
 											<div class="job-primary">
-												<h2 class="job-title"><a href="single.php?job-id=<?php echo $jobs['id']; ?>"><?php echo $jobs['title']?></a></h2>
+												<h2 class="job-title"><a href="single.php?job_id=<?php echo $jobs['id']; ?>"><?php echo $jobs['title']?></a></h2>
 												<div class="job-meta">
-													<a class="meta-company" href="#">Company Awesome Ltd.</a>
+													<a class="meta-company" href="#"><?php echo $jobs["company_name"]; ?></a>
 													<span class="meta-date">Posted on: <?php echo $jobs['date_posted'] ?></span>
 												</div>
 												<div class="job-details">
