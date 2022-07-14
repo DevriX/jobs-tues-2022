81e8a1d839ff90fad197f245f246776a39d9fd21
skrrrrrrrrrrrrrt <iliqnemilov11@gmail.com>
2022-07-14 07:55:01 -0700

    remoed var dump

--- config.php
+++ config.php
@@ -1,17 +1,7 @@
 <?php
 
-<<<<<<< Updated upstream
-define("USER", "fustuk");
-define("PASSWORD", "123456");
-define("HOST", "localhost");
-define("DB_NAME", "jobs_devrix");
-define('RES_LIMIT', 5);
-
-
-=======
 define("USER", "");
 define("PASSWORD", "");
 define("HOST", "");
 define("DB_NAME", "");
 define('RES_LIMIT', 5);
->>>>>>> Stashed changes
