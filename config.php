a632de9afe3087501cf3a182324739265406c5c3
skrrrrrrrrrrrrrt <iliqnemilov11@gmail.com>
2022-07-14 07:54:07 -0700

    fixed config

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< Updated upstream
 define("USER", "fustuk");
 define("PASSWORD", "123456");
 define("HOST", "localhost");
@@ -7,3 +8,10 @@ define("DB_NAME", "jobs_devrix");
 define('RES_LIMIT', 5);
 
 
+=======
+define("USER", "");
+define("PASSWORD", "");
+define("HOST", "");
+define("DB_NAME", "");
+define('RES_LIMIT', 5);
+>>>>>>> Stashed changes
