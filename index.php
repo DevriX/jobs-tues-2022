e0f6547bfe595208d83a6ba8463063d4d7b5a92a
abozhiinov <abozhiinov@gmail.com>
2022-07-15 16:36:47 +0300

    final change

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php include 'header.php';
-	if(isset($_GET['page'])){
-		if($_GET['page'] > 1 && isset($_GET['filter'])){
-			header("Location: ".urldecode("?".remove_page_param_from_url()));
-		}
-	}
-?>
+<?php include 'header.php'; ?>
 
 <body>
 
