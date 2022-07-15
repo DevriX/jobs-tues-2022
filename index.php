7969f9834829dc13cf7305d66de79b087c029789
Alexandra Stancheva <66909315+allks12@users.noreply.github.com>
2022-07-15 12:14:10 +0300

    Moved header location to the top of the file.

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php include 'header.php';?>
+<?php include 'header.php';
+	if(isset($_GET['page'])){
+		if($_GET['page'] > 1 && isset($_GET['filter'])){
+			header("Location: ".urldecode("?".remove_page_param_from_url()));
+		}
+	}
+?>
 
 <body>
 
@@ -12,11 +18,6 @@
 				<div class="row">
 					<form method = "get">
 						<?php
-						if(isset($_GET['page'])){
-							if($_GET['page'] > 1 && isset($_GET['filter'])){
-								header("Location: ".urldecode("?".remove_page_param_from_url()));
-							}
-						}
 						if(isset($_GET['filter'])){
 						
 							foreach($_GET['filter'] as $filter){
