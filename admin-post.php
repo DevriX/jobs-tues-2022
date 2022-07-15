c9deb31c653f6713622b8d201193e0e2ed422977
abozhiinov <abozhiinov@gmail.com>
2022-07-15 15:18:55 +0300

    PREPARED STATEMENTS

--- admin-post.php
+++ admin-post.php
@@ -3,51 +3,37 @@
 include 'header.php';
 
 if ($_POST['action'] == 'approve') {
-    $approve_request = 
-        "UPDATE jobs SET status = 1 WHERE id = " . $_POST['job'] . "";
-
-    if ($conn->query($approve_request) === FALSE) {
-        echo "Error: " . $approve_request . "<br>" . $conn->error;
-    }
+    $stmt = $conn->prepare("UPDATE jobs SET status = 1 WHERE id = ?");
+	$stmt->bind_param('s', $_POST['job']);
+	$stmt->execute();
 }
 
 if ($_POST['action'] == 'reject') {
-    $approve_request = 
-        "UPDATE jobs SET status = 0 WHERE id = " . $_POST['job'] . "";
-
-    if ($conn->query($approve_request) === FALSE) {
-        echo "Error: " . $approve_request . "<br>" . $conn->error;
-    }
+    $stmt = $conn->prepare("UPDATE jobs SET status = 0 WHERE id = ?");
+	$stmt->bind_param('s', $_POST['job']);
+	$stmt->execute();
 }
 
 if($_POST['action'] == 'delete'){
-    $delete_categories_request = 
-        "DELETE FROM jobs_categories
-         WHERE job_id=" . $_POST['job'] . " ";
-    if ($conn->query($delete_categories_request) === FALSE) {
-        echo "Error: " . $delete_categories_request . "<br>" . $conn->error;
-    }
+    $stmt = $conn->prepare("DELETE FROM jobs_categories
+                            WHERE job_id=?");
+	$stmt->bind_param('s', $_POST['job']);
+	$stmt->execute();
 
-    $delete_appliciants_request = 
-        "DELETE FROM applications 
-         WHERE job_id = " . $_POST['job'] . " ";
-    if ($conn->query($delete_appliciants_request) === FALSE) {
-        echo "Error: " . $delete_appliciants_request . "<br>" . $conn->error;
-    }
+    $stmt = $conn->prepare("DELETE FROM applications
+                            WHERE job_id=?");
+	$stmt->bind_param('s', $_POST['job']);
+	$stmt->execute();
 
-    $delete_job_request = 
-        "DELETE FROM jobs 
-         WHERE jobs.id=" . $_POST['job'] . " ";
-    if ($conn->query($delete_job_request) === FALSE) {
-        echo "Error: " . $delete_job_request . "<br>" . $conn->error;
-    }
+    $stmt = $conn->prepare("DELETE FROM jobs
+                            WHERE id=?");
+	$stmt->bind_param('s', $_POST['job']);
+	$stmt->execute();
 }
 
 if($_POST['action'] == 'delete-submission'){
-    $delete_submission_request = 
-        "DELETE FROM applications 
-         WHERE applications.id=" . $_POST['application'] . " ";
-    if ($conn->query($delete_submission_request) === FALSE) {
-        echo "Error: " . $delete_submission_request . "<br>" . $conn->error;
-    }
+    $stmt = $conn->prepare("DELETE FROM applications
+                            WHERE applications.id=?");
+	$stmt->bind_param('s', $_POST['application']);
+	$stmt->execute();
 }
